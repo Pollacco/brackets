1851cbb596d9bc82fd77e2843c3d709e87cf1fde
Andrey <andrey.sviontek@gmail.com>
2019-03-18 23:18:56 +0500

    feat: first try to complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let bracketsArrow = str.split('');
+  // let open = ['[', '(', '{'];
+  // let close = [']', ')', '}'];
+  let stack = [];
+  let bracketsConfig = [];
+
+  for (let i = 0; i < bracketsArrow.length; i++) {
+    if (bracketsArrow[i] === '[' ||
+    bracketsArrow[i] === '(' ||
+    bracketsArrow[i] === '{' )
+    stack.push(bracketsArrow[i]);
+  }  else if (bracketsArrow[i] === ']' && bracketsArrow[i - 1] === '[' ||
+    bracketsArrow[i] === ')' && bracketsArrow[i - 1] === '('||
+    bracketsArrow[i] === '}' && bracketsArrow[i - 1] === '}')
+    {
+      if stack
+    }
+    stack.pop(bracketsArrow[i - 1]);
+
+  
+
+  console.log(str);
+  console.log('stack:' + stack);
 }
+
+check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
